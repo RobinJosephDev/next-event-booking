f1b653d6e733e3b53554ab64f4dea6215cf79fce
Robin Joseph <robinjo1776@gmail.com>
2026-01-10 13:04:28 +0530

    updated db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,18 +1,18 @@
 import pkg from "pg";
 const { Pool } = pkg;
 
-const isProduction = process.env.NODE_ENV === "production";
-
 const pool = new Pool({
-  connectionString: isProduction
+  connectionString: process.env.DATABASE_URL
     ? process.env.DATABASE_URL
     : `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
-  ssl: isProduction ? { rejectUnauthorized: false } : false,
+  ssl: process.env.DATABASE_URL
+    ? { rejectUnauthorized: false }
+    : false,
 });
 
 pool
   .connect()
   .then(() => console.log("✅ Database connected successfully"))
-  .catch((err) => console.error("❌ Database connection error:", err.stack));
+  .catch((err) => console.error("❌ Database connection error:", err));
 
 export default pool;
