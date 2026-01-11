4dfeb5263bc62fbbf21f61a5a644f7289f9bb63f
Robin Joseph <robinjo1776@gmail.com>
2026-01-11 21:15:07 +0530

    updated code

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,22 +1,11 @@
-import dotenv from "dotenv";
-dotenv.config({ path: "./.env" });
-
 import pkg from "pg";
 const { Pool } = pkg;
 
-console.log("DB_USER =", process.env.DB_USER);
-console.log("DB_PASSWORD =", process.env.DB_PASSWORD);
-console.log("DB_PASSWORD type =", typeof process.env.DB_PASSWORD);
-console.log("DB_HOST =", process.env.DB_HOST);
-console.log("DB_NAME =", process.env.DB_NAME);
-
-
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === "production"
+    ? { rejectUnauthorized: false }
+    : false,
 });
 
 pool
