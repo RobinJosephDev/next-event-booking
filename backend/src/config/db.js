6edbc877b6574bce2a698bc561c0b16a9d1c4a4e
Robin Joseph <robinjo1776@gmail.com>
2026-01-21 13:12:01 +0530

    updated proj

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,11 +1,16 @@
 import pkg from "pg";
 const { Pool } = pkg;
 
+import dotenv from "dotenv";
+dotenv.config();
+
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === "production"
-    ? { rejectUnauthorized: false }
-    : false,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD, // ✅ must be a string
+  database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT),
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
 });
 
 pool
